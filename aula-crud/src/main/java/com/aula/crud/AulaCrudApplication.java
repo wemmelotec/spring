44e40422d13af6b244b2c0e67cc763c79080b2cd
wemmelotec <tonmelo@hotmail.com>
2022-01-05 11:10:52 -0300

    crud

---
+++ aula-crud/src/main/java/com/aula/crud/AulaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.aula.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaCrudApplication.class, args);
+	}
+
+}
---
+++ aula-crud/src/test/java/com/aula/crud/AulaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
